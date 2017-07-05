3606b4066f18bbcaffc3d89488791fb3ee014d9e
anderson06 <andersoncampos.m@gmail.com>
2017-07-05 01:16:18 -0300

    Add deploy command

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,41 @@
 const webpack = require('webpack');
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const definePlugin = new webpack.DefinePlugin({
+  'process.env': {
+    NODE_ENV: JSON.stringify('production')
+  }
+});
+
+const uglifyPlugin = new UglifyJSPlugin();
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].[contenthash].css",
+    disable: process.env.NODE_ENV !== "production"
+});
+
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './public/index.html',
   filename: 'index.html',
   inject: 'body'
-})
+});
+
+const plugins = [HtmlWebpackPluginConfig, extractSass];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(uglifyPlugin);
+  plugins.push(definePlugin);
+}
 
 const config = {
   context: __dirname,
   entry: ['./src/index.jsx'],
   output: {
     filename: 'main.js',
-    path: path.resolve('public')
+    path: path.resolve('dist')
   },
   resolve: {
     extensions: ['.js', '.jsx']
@@ -32,15 +54,17 @@ const config = {
       },
       {
         test: /\.scss$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-          { loader: "sass-loader" }
-        ]
+        use: extractSass.extract({
+          use: [
+            { loader: "css-loader" },
+            { loader: "sass-loader" }
+          ],
+          fallback: "style-loader"
+        })
       }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig],
+  plugins: plugins,
 }
 
 module.exports = config;
