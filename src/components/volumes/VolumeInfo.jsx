dfd96a80d418c64aed6859b340db8e422d13c7f4
anderson06 <andersoncampos.m@gmail.com>
2017-07-12 02:28:47 -0300

    Centralize loading

--- src/components/volumes/VolumeInfo.jsx
+++ src/components/volumes/VolumeInfo.jsx
@@ -43,16 +43,16 @@ class VolumeInfo extends Component {
 
   renderLoading() {
     return (
-      <div className="preloader-wrapper big active fb-loading">
-        <div className="spinner-layer spinner-blue">
-          <div className="circle-clipper left">
-            <div className="circle"></div>
-          </div>
-          <div className="gap-patch">
-            <div className="circle"></div>
-          </div>
-          <div className="circle-clipper right">
-            <div className="circle"></div>
+      <div className="col s12 m4 center">
+        <div className="preloader-wrapper active">
+          <div className="spinner-layer spinner-red-only">
+            <div className="circle-clipper left">
+              <div className="circle"></div>
+              </div><div className="gap-patch">
+              <div className="circle"></div>
+              </div><div className="circle-clipper right">
+              <div className="circle"></div>
+            </div>
           </div>
         </div>
       </div>
